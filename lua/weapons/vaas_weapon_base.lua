1642d7ad3afc0a92445ff2682ec2f5445c1719bb
J Mason <nosamrj@gmail.com>
2019-03-09 14:09:11 -0800

    Update vaas_weapon_base.lua

--- lua/weapons/vaas_weapon_base.lua
+++ lua/weapons/vaas_weapon_base.lua
@@ -6,9 +6,7 @@ if(SERVER)then
 	--missing alot of addons most likely for starwars weapons. just copy each addons ID into here.
 
 	--[[DO ***NOT*** PUT THEM IN YOUR CONTENT COLLECTION, EXTRACT THEM AND UPLOAD THEM TO YOUR SERVER INSTEAD AND REMOVE THE LUA CODE FROM THEM!!!! ITS BETTER TO JUST REMAKE THEM FROM SCRATCH!!!!
-
 	JUST MAKE SURE YOU KNOW HOW TO MAKE A BASIC SWEP FIRST AND YOU'LL BE FINE.
-
 	YOU SHOULD BE DOING THIS ANYWAYS FOR YOUR ADDONS ALREADY BTW!!! ;)
 	]]
 end
@@ -17,87 +15,64 @@ AddCSLuaFile()
 
 --[[SWEP CONFIGURATION SETTINGS, MAKE A NEW LUA FILE FOR YOUR SWEPS AND COPY THE CONFIGE BELOW AND EDIT IT TO YOUR LIKING]]
 
---[[SWEP AUTHOR NAME HERE]]
-SWEP.Author = "Vaas Kahn Grim"
---[[CHANGE THIS TO "vaas_weapon_base" when you make your new sweps with it]]
-SWEP.Base = "weapon_base"
---[[name of the weapon]]
-SWEP.PrintName = "Vaas Weapon Base"
---[[What ever you want players to know]]
-SWEP.Instructions = "Example"
---[[what you see in first person view]]
-SWEP.ViewModel = "models/weapons/v_DC15A.mdl"
---[[if its upside down in first person set this to true]]
-SWEP.ViewModelFlip = false
---[[keep this false unless you don't see hands properly]]
-SWEP.UseHands = false
---[[what other players see you holding]]
-SWEP.WorldModel = "models/weapons/W_DC15A.mdl"
---[[set to "ar2" for rifle like weapons, set to "pistol" for pistols.]]
-SWEP.SetHoldType = "ar2"
---[[set to same as above just in case something tries to overide SWEP.SetHoldType]]
-SWEP.DEFAULTHOLD = "ar2"
---[[how heavy your swep is? I guess it could be useful so why the hell not put it here for you]]
-SWEP.Weight = 5
---[[switch to this weapon when you pick it up?]]
-SWEP.AutoSwitchTo = true
---[[switch from this weapon when you pick up another?]]
-SWEP.AutoSwitchFrom = false
---[[which key do you want to press to get to this weapon? 1,2,3,4,5 or 6]]
-SWEP.Slot = 0
---[[basically sort order of weapons on the same slot. 0 will be highest and 99 at botem of list]]
-SWEP.SlotPos = 0
---[[apparently important. I suggest leave false unless you think it needs to be true]]
-SWEP.DrawAmmo = false
---[[a crosshair thats basic and default with garrysmod. true if you want it, false if you don't]]
-SWEP.DrawCrosshair = true
---[[where it will be in the q-menu]]
-SWEP.Category = "Vaas Weapon Base"
---[[will it even show up in q-menu?]]
-SWEP.Spawnable = true
---[[is it only spawnable by BADMINS]]
-SWEP.AdminSpawnable = true
---[[the laser that actually fires out the gun. don't fuck with this unless you know how effects work in lua]]
-SWEP.TracerName = "blue"
---[[its a laser gun, best not to change the tracer count unless its a Republic shotgun or something]]
-SWEP.TracerCount = 1
---[[how much ammo per clip]]
-SWEP.Primary.ClipSize = 50
---[[ammo in reserve]]
-SWEP.Primary.DefaultClip = 200
---[[ammo type, leave the same or getting ammo is a bitch for players]]
-SWEP.Primary.Ammo = "ar2"
---[[single shot or automatic? will have better method later for this]]
-SWEP.Primary.Automatic = true
---[[how much damage for each shot]]
-SWEP.Primary.Damage = 10
---[[how many blasts come out per shot? leave as 1 unless its a shotgun or something]]
-SWEP.Primary.NumShots = 1
---[[how much it spreads out when firing. keep in mind that its easy to fuck up this one. keep the number small]]
-SWEP.Primary.Spread = 0.02
---[[noticed nothing changing from this so ignore unless you know what to do with it.]]
-SWEP.Primary.Cone = 0
---[[how long between each shot being fired from the gun. lower is faster]]
-SWEP.Primary.Delay = 0.2
---[[how long it takes to begin reloading the gun. For later features to use. leave default for now]]
-SWEP.ReloadDelay = 0.5
---[[how far you zoom in? will redo this later probably]]
-SWEP.ZoomLevelA = 60
---[[how fast you zoom in]]
-SWEP.ZoomLevelB = .2 --zoom speed
---[[can you zoom in at all?]]
-SWEP.zoomEnabled = true
---[[don't bother changing the sound for reloading unless you really want to]]
-SWEP.ReloadSound = Sound("weapons/shared/standard_reload.ogg")
+
+SWEP.Base = "weapon_base"--[[KEEP THIS AS"vaas_weapon_base" when you make new sweps with it!]]
+
+SWEP.Author = "Vaas Kahn Grim" --[[SWEP AUTHOR NAME HERE]]
+SWEP.PrintName = "Template" --[[name of the weapon]]
+SWEP.Instructions = "Example" --[[What ever you want players to know]]
+SWEP.Category = "Vaas Weapon Base" --[[where it will be in the q-menu; Try to keep it in the same category of similiar weapons. Check around for whats in use!]]
+
+SWEP.Spawnable = false --[[will it even show up in q-menu?]]
+SWEP.AdminSpawnable = false --[[is it only spawnable by BADMINS]]
+
+SWEP.ViewModel = "models/weapons/v_DC15A.mdl" --[[what you see in first person view]]
+SWEP.ViewModelFlip = false --[[if its upside down in first person set this to true]]
+SWEP.WorldModel = "models/weapons/W_DC15A.mdl" --[[what other players see you holding]]
+SWEP.UseHands = false --[[keep this false unless you don't see hands properly]]
+
+SWEP.SetHoldType = "ar2" --[[set to "ar2" for rifle like weapons, set to "pistol" for pistols.]]
+SWEP.DEFAULTHOLD = "ar2" --[[set to same as above just in case something tries to overide SWEP.SetHoldType]]
+
+SWEP.AutoSwitchTo = true --[[switch to this weapon when you pick it up?]]
+SWEP.AutoSwitchFrom = false--[[switch from this weapon when you pick up another?]]
+SWEP.ShouldDropOnDie = false --[[do you want to drop it on death? not sure what any starwarsRP server would want that considering lots of minges join servers to RDM]]
+
+SWEP.Slot = 0 --[[which key do you want to press to get to this weapon? 1,2,3,4,5 or 6]] 
+SWEP.SlotPos = 0 --[[basically sort order of weapons on the same slot. 0 will be highest and 99 at botem of list]]
+
+SWEP.DrawAmmo = false --[[apparently important. I suggest leave false unless you think it needs to be true]]
+SWEP.DrawCrosshair = true --[[a crosshair thats basic and default with garrysmod. true if you want it, false if you don't]]
+
+SWEP.Weight = 5 --[[how heavy your swep is? I guess it could be useful so why the hell not put it here for you]]
+SWEP.TracerName = "blue" --[[the laser that actually fires out the gun. don't fuck with this unless you know how effects work in lua]]
+SWEP.TracerCount = 1 --[[its a laser gun, best not to change the tracer count unless its a Republic shotgun or something]]
+SWEP.Primary.NumShots = 1 --[[how many blasts come out per shot? leave as 1 unless its a shotgun or something]]
+
+SWEP.Primary.Automatic = true --[[single shot or automatic? will have better method later for this]]
+SWEP.Primary.ClipSize = 50 --[[how much ammo per clip]]
+SWEP.Primary.DefaultClip = 200 --[[ammo in reserve]]
+SWEP.Primary.Ammo = "ar2" --[[ammo type, leave the same or getting ammo is a bitch for players]]
+
+SWEP.Primary.Damage = 10 --[[how much damage for each shot]]
+SWEP.Primary.Spread = 0.02 --[[how much it spreads out when firing. keep in mind that its easy to fuck up this one. keep the number small]]
+SWEP.Primary.Cone = 0 --[[noticed nothing changing from this so ignore unless you know what to do with it.]]
+SWEP.Primary.Delay = 0.2 --[[how long between each shot being fired from the gun. lower is faster]]
+
+SWEP.ZoomLevelA = 60 --[[how far you zoom in? will redo this later probably]]
+SWEP.ZoomLevelB = .2 --[[how fast you zoom in]]
+SWEP.zoomEnabled = true --[[can you zoom in at all?]]
+
+local ShootSound = Sound("weapons/dc15a/dc15a_fire.ogg") --[[the sound of the gun when you shoot it]]
+SWEP.ReloadSound = Sound("weapons/shared/standard_reload.ogg") --[[don't bother changing the sound for reloading unless you really want to]]
+
+
 --[[leave these unchanged for now, will probably rework later for other uses]]
+SWEP.ReloadDelay = 0.5 --[[how long it takes to begin reloading the gun. For later features to use. leave default for now]]
 SWEP.Secondary.ClipSize = -1
 SWEP.Secondary.DefaultClip = -1
 SWEP.Secondary.Ammo = "none"
 SWEP.Secondary.Automatic = false
---[[do you want to drop it on death? not sure what any starwarsRP server would want that considering lots of minges join servers to RDM]]
-SWEP.ShouldDropOnDie = false
---[[the sound of the gun when you shoot it]]
-local ShootSound = Sound("weapons/dc15a/dc15a_fire.ogg")
 
 -- removed the SWEp.FireTypes
 
@@ -109,11 +84,9 @@ SWEP.ExtraModels = {
 	//////////////////////////////////////////////////////////////////
 	////////////////////END OF CONFIGURATION//////////////////////////
 	//////////////////////////////////////////////////////////////////
-
 	EVERYTHING BELOW IS SETUP THE WAY IT NEEDS TO BE TO WORK WITH YOUR
 	NEW SWEPS YOU MAKE! DO NOT COPY ANYTHING BELOW THIS INTO YOUR NEW
 	SWEPS!!! ITS ALREADY BEING USED WHEN YOU SET SWEP.Base = "vaas_weapon_base"
-
 	Seriously I made this thing easy to use for a reason, its basic as
 	fuck but atleast you won't have the insane retarded issues that alot
 	of people have with TFA Weapons. I'll update it later with new features
@@ -121,7 +94,6 @@ SWEP.ExtraModels = {
 	config for each swep. I included some somewhat helpful advice for each
 	entry in the config. Lastly don't forget to follow the instructions at
 	the top regarding the downloading of content for this.
-
 										With Lua,
 													Vaas Kahn Grim
 													[RAPADANT NETWORKS]
