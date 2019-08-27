afc8dd46ba0ae0f98d6da4da874dce61f7f8db27
Vaas_Kahn_Grim <29955667+VaasKahnGrim@users.noreply.github.com>
2019-08-27 15:32:11 -0400

    Update vaas_weapons_updatechecker.lua

--- lua/autorun/vaas_weapons_updatechecker.lua
+++ lua/autorun/vaas_weapons_updatechecker.lua
@@ -2,7 +2,7 @@ local update_info_tbl = util.JSONToTable(http.Fetch("https://raw.githubuserconte
 local current_version = "Beta 3A"
 if update_info_tbl.version != current_version then
 	print("Vaas Weapon Base version didn't match the version check. Current version on server: "..current_version)
-	print("The currently uptodate version: "..update_info_tbl.version.." Version #: "..update_info_tbl.version_num)
-	print("You can find the current version here https://github.com/VaasKahnGrim/BASIC_StarwarsRP_Weapon_Base")
+	--print("The currently uptodate version: "..update_info_tbl.version.." Version #: "..update_info_tbl.version_num)
+	--print("You can find the current version here https://github.com/VaasKahnGrim/BASIC_StarwarsRP_Weapon_Base")
 	print("Or here https://steamcommunity.com/sharedfiles/filedetails/?id=1548256366")
 end
